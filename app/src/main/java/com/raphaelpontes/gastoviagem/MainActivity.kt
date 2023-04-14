0311a5d3e049e1cc04a5d0c4c372c5e3f8124e30
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-04-14 11:47:56 -0300

    fix problems

--- app/src/main/java/com/raphaelpontes/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/raphaelpontes/gastoviagem/MainActivity.kt
@@ -2,10 +2,7 @@ package com.raphaelpontes.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
 import com.raphaelpontes.gastoviagem.databinding.ActivityMainBinding
 
@@ -42,7 +39,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val price: Float = binding.editPrice.text.toString().toFloat()
             val autonomy: Float = binding.editAutonomy.text.toString().toFloat()
             val totalValue: Float = (distance * price / autonomy)
-            val totalValueStr: String = "R$ ${"%.2f".format(totalValue)}"
+            val totalValueStr = "R$ ${"%.2f".format(totalValue)}"
             binding.textTotalValue.text = totalValueStr
         } else {
             Toast.makeText(this, R.string.validate_fill_all_fields, Toast.LENGTH_SHORT).show()
